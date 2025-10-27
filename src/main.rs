c6e735526a3f9cbd6fa14b948b55cbec78f8adfc
SabaliDev <127083776+SabaliDev@users.noreply.github.com>
2025-10-27 03:27:26 +0300

    market -orders

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,29 @@ mod matching_engine;
 use matching_engine::engine::MatchingEngine;
 use matching_engine::orderbook::{Order, BidOrAsk,OrderBook};
 
+use crate::matching_engine::engine::TradingPair;
+use rust_decimal_macros::dec;
+
 fn main() {
     
     let buy_order_alice = Order::new(BidOrAsk::Bid,5.5);
     let buy_order_bob = Order::new(BidOrAsk::Bid,2.45);
 
     let mut order_book = OrderBook::new();
-    order_book.add_order(4.4,buy_order_alice);
-    order_book.add_order(4.4,buy_order_bob);
+    order_book.add_limit_order(dec!(4.4),buy_order_alice);
+    order_book.add_limit_order(dec!(4.4),buy_order_bob);
 
     let sell_order = Order::new(BidOrAsk::Ask, 6.5);
-    order_book.add_order(20.0,sell_order);
+    order_book.add_limit_order(dec!(20.0),sell_order);
 
-    println!("{:#?}", order_book);
+    //println!("{:#?}", order_book);
 
-    let engine = MatchingEngine::new();
+    let mut engine = MatchingEngine::new();
+    let pair  = TradingPair::new("BTC".to_string(),"USD".to_string());
+    engine.add_new_market(pair.clone());
+
+    let buy_order = Order::new(BidOrAsk::Bid,6.5);
+    engine.place_limit_order(pair,dec!(10.000),buy_order).unwrap()
+
+    
 }
--- src/matching_engine/engine.rs
+++ src/matching_engine/engine.rs
@@ -1,7 +1,9 @@
-use super::orderbook::OrderBook;
-use std::collections::HashMap;
+#![allow(dead_code)]
+use super::orderbook::{OrderBook,Order};
+use std::{collections::HashMap};
+use rust_decimal::prelude::*;
 
-
+#[derive(PartialEq, Eq, Hash, Clone,Debug)]
 pub struct TradingPair{
     base: String,
     quote: String
@@ -13,8 +15,11 @@ impl TradingPair {
             base, quote
         }
     }
+    pub fn to_string(&self) ->String{
+        format!("{}/{}",self.base,self.quote)
+    }
 }
-
+#[derive(Debug)]
 pub struct MatchingEngine {
     orderbooks: HashMap<TradingPair,OrderBook>
 }
@@ -24,5 +29,24 @@ impl MatchingEngine{
     MatchingEngine{
         orderbooks:HashMap::new(),
     }
- }   
+ }
+ pub fn add_new_market(&mut self, pair: TradingPair){
+    self.orderbooks.insert(pair.clone(), OrderBook::new());
+    print!("Opening new orderbook for market {:?}", pair.to_string());
+ }
+ pub fn place_limit_order(&mut self, pair: TradingPair, price:Decimal, order:Order) -> Result<(),String>{
+    match self.orderbooks.get_mut(&pair){
+        Some(orderbook) => {
+            orderbook.add_limit_order(price,order);
+
+            println!("Placed limit order @ price {}", price);
+            Ok(())
+        }
+        None => {
+            Err(format!("The order book for the given trading pair ({})does not exist",pair.to_string()))
+        }
+    }
+
+   
+ }
 }
